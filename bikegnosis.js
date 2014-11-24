c848cc3b0eb57dbe99966ba19b798d5561c9c47f
nathanallen <thenathanator@gmail.com>
2014-11-24 14:37:23 -0800

    add riot.js render + display problems w/ images

--- bikegnosis.js
+++ bikegnosis.js
@@ -2,6 +2,29 @@ $(document).ready(function(){
     render_page()
 })
 
+// render function taken from riot.js framework
+var FN = {}, // Precompiled templates (JavaScript functions)
+  template_escape = {"\\": "\\\\", "\n": "\\n", "\r": "\\r", "'": "\\'"},
+  render_escape = {'&': '&amp;', '"': '&quot;', '<': '&lt;', '>': '&gt;'};
+
+function default_escape_fn(str, key) {
+  return str == null ? '' : (str+'').replace(/[&\"<>]/g, function(char) {
+    return render_escape[char];
+  });
+}
+
+function render(tmpl, data, escape_fn) {
+  if (escape_fn === true) escape_fn = default_escape_fn;
+  tmpl = tmpl || '';
+
+  return (FN[tmpl] = FN[tmpl] || new Function("_", "e", "return '" +
+    tmpl.replace(/[\\\n\r']/g, function(char) {
+      return template_escape[char];
+    }).replace(/{\s*([\w\.]+)\s*}/g, "' + (e?e(_.$1,'$1'):_.$1||(_.$1==null?'':_.$1)) + '") + "'")
+  )(data, escape_fn);
+};
+// end riot.js render
+
 function build_checklist_section(section_title, section_values) {
     var title = "<h2>" + section_title + "</h2>",
         new_elements = [title],
@@ -14,8 +37,23 @@ function build_checklist_section(section_title, section_values) {
     return new_elements
 }
 
+function build_problem_summary(problems_tmpl, filtered_problems) {
+    var n = filtered_problems.length,
+        new_elements = []
+
+    for (var i = 0; i < n; i++ ){
+        new_elements.push(render(problems_tmpl, filtered_problems[i]))
+    }
+    return new_elements
+}
+
 function render_page() {
-    var $form = $('form')
-    $form.append(build_checklist_section("Sound Type", sounds))
-    $form.append(build_checklist_section("Rhythm Type", rhythms))
+    var $form = $('form'),
+        $problems = $('div#problems'),
+        problems_tmpl = $('#problem-item').html()
+
+    $form.find('.pull_left').append(build_checklist_section("Sound Type", sounds))
+                       .append(build_checklist_section("Rhythm Type", rhythms))
+    // $form.find('.pull_right')
+    $problems.append(build_problem_summary(problems_tmpl, problems))
 }
--- problems.js
+++ problems.js
@@ -4,12 +4,12 @@ var rhythms = ["regular", "irregular", "intermittent"],
     revolution_types = ["wheel", "crank", "chain"]
     worse_whens = ["climbing", "standing_and_pedaling", "cornering", "road_is_rough", "coasting", "braking", "pedaling", "accelerating"].sort()
 var problems = [
-  {description: "broken spoke", sounds: ["clicking", "creaking", "pinging", "popping", "ticking", "rattling", "rubbing"], rhythm: {regular: true, revolution_type: "wheel"}, worse_when: {climbing: true, standing_and_pedaling: true} },
-  {description: "low spoke tension", sounds: ["clicking", "creaking", "pinging", "popping", "ticking", "rubbing"], rhythm: {intermittent: true, revolution_type: "wheel"} },
-  {description: "stiff chainlink", sounds: ["clicking"], rhythm: {regular: true, intermittent: true, revolution_type: "chain"} },
-  {description: "loose handlebar binder bolt", sounds: ["clicking", "creaking", "ticking", "tapping"], rhythm: {intermittent: true, revolution_type: "crank"}, worse_when: {climbing: true, standing_and_pedaling: true} },
-  {description: "crooked brake caliper", sounds: ["rubbing"], rhythm: {regular: true, revolution_type: "wheel"} },
-  {description: "rubbing kickstand", sounds: ["rubbing", "buzzing"], rhythm: {regular: true, revolution_type: "wheel"} },
-  {description: "hitting kickstand", sounds: ["rubbing", "clunking", "tapping"], rhythm: {regular: true, revolution_type: "crank"}, worse_when: {pedaling: true} }//,
-  // {description: "Is your...", sounds: [], rhythm: {regular: true, irregular: false, intermittent: true, revolution_type: ""}, worse_when: {} },
+  {description: "broken spoke", sounds: ["clicking", "creaking", "pinging", "popping", "ticking", "rattling", "rubbing"], rhythm: {regular: true, revolution_type: "wheel"}, worse_when: {climbing: true, standing_and_pedaling: true}, image_url: "http://brimages.bikeboardmedia.netdna-cdn.com/wp-content/uploads/2009/06/mavic-2nd-gen-r-sys-wheel-break.jpg" },
+  {description: "low spoke tension", sounds: ["clicking", "creaking", "pinging", "popping", "ticking", "rubbing"], rhythm: {intermittent: true, revolution_type: "wheel"}, image_url: "http://ep1.pinkbike.org/p4pb7605197/p4pb7605197.jpg" },
+  {description: "stiff chainlink", sounds: ["clicking"], rhythm: {regular: true, intermittent: true, revolution_type: "chain"}, image_url: "http://www.atomiczombie.com/tutorials/Chain%20Link%20Tool/Figure%2020.jpg" },
+  {description: "loose handlebar binder bolt", sounds: ["clicking", "creaking", "ticking", "tapping"], rhythm: {intermittent: true, revolution_type: "crank"}, worse_when: {climbing: true, standing_and_pedaling: true}, image_url: "http://re-cycle.com/images/Lambert/bolt.jpg"},
+  {description: "crooked brake caliper", sounds: ["rubbing"], rhythm: {regular: true, revolution_type: "wheel"}, image_url: "http://17rg073sukbm1lmjk9jrehb643.wpengine.netdna-cdn.com/wp-content/uploads/2014/02/caliper-225x300.jpg"},
+  {description: "rubbing kickstand", sounds: ["rubbing", "buzzing"], rhythm: {regular: true, revolution_type: "wheel"}, image_url: "http://1.bp.blogspot.com/-pcoWQSQZrWU/UBXc6Ai-VVI/AAAAAAAAEV0/zlA3vhc6gus/s1600/DSCF5164.jpg"},
+  {description: "hitting kickstand", sounds: ["rubbing", "clunking", "tapping"], rhythm: {regular: true, revolution_type: "crank"}, worse_when: {pedaling: true}, image_url: "http://4.bp.blogspot.com/-JYl4phRwhyM/UUEPB8_j7bI/AAAAAAAANcs/zMbjLfbEeD0/s1600/DSCF7185.jpg"  }//,
+  // {description: "Is your...", sounds: [], rhythm: {regular: true, irregular: false, intermittent: true, revolution_type: ""}, worse_when: {}, image_url: ""  },
 ]
